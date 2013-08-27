b9921eba5f7371d29e9ad2ee8437e787bf46d21a
angelos-se <Angelos@Kana.local>
2013-08-28 02:45:12 +0800

    Now reads output from diskutil
    Also added ver check for OS X and less verbose output

--- dev.py
+++ dev.py
@@ -6,13 +6,22 @@ def main():
     DiskList = []
 
     # Part 0
-    uname = os.uname()
-    DEVNULL = open(os.devnull, 'wb')
-    haveDiskutil = not subprocess.call(['which', 'diskutil'], stdout=DEVNULL, stderr=subprocess.STDOUT)
+    try: osxVerNum = subprocess.check_output("sw_vers | grep 10. | awk '{ FS = \" \" } ; { print $2 }'", shell=True).strip().split('.')
+    except: osxVer = False
+    else:
+    	try: osxVer = int(osxVerNum[1]) >= 8 and int(osxVerNum[2]) >= 0
+    	except: osxVer = False
+
+    try:
+    	subprocess.check_output(['which', 'which'])
+    	try: haveDiskutil = 'diskutil' in subprocess.check_output(['which', 'diskutil'])
+    	except: haveDiskutil = False
+    except: osxVer = False
+    
     pyVer27 = sys.version_info[0] == 2 and sys.version_info[1] >= 7
 
-    if uname[0] != 'Darwin':
-        print 'Not Darwin! Are you really running this on OS X?'
+    if os.uname()[0] != 'Darwin' or not osxVer:
+        print 'Not OS X or version too old or missing UNIX tools...'
         sys.exit(1)
     elif not haveDiskutil:
         print 'Diskutil not found...'
@@ -20,8 +29,7 @@ def main():
     elif not pyVer27:
         print 'Python version too old or too new...'
         sys.exit(3)
-    else:
-        print 'Good, we are on Darwin, found Disk Utility and have Python 2.7 or better!'
+    #else: print 'Good, we are on Darwin, found Disk Utility and have Python 2.7 or better!'
 
     # Part 1
     print 'Current working directory is: ', os.getcwd()
@@ -31,12 +39,21 @@ def main():
 
     # Part 2
     import xml.etree.ElementTree as plist
-    DiskPlist = plist.parse('Disks.plist').getroot() # Hard coded temporarily
-
-    if DiskPlist[0][6].text != 'WholeDisks':
-        print 'Diskutil generated unexpected output, exiting...'
-        sys.exit(4)
-    for disk in DiskPlist[0][7]: DiskList.append(disk.text)
-    print 'Found disks: ', DiskList
+    try: output = subprocess.check_output(['diskutil', 'list', '-plist'])
+    except: output = ''
+    else:
+    	try: 
+    		DiskPlist = plist.fromstring(output)
+    	except:
+    		print 'Unable to parse Diskutil output, exiting...'
+        	sys.exit(4)
+    	else: 
+    		if DiskPlist[0][6].text != 'WholeDisks':
+        		print 'Diskutil generated unexpected output, exiting...'
+        		sys.exit(5)
+	
+	#DiskPlist = plist.parse('Disks.plist').getroot() # Hard coded temporarily	
+	for disk in DiskPlist[0][7]: DiskList.append(disk.text)
+	print 'Found disks: ', DiskList
 
 if __name__ == "__main__": main()
