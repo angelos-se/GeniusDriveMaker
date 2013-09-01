a8176f8eca0b15069892de7c7ea9dc10eda87e4b
angelos-se <Angelos@Kana.local>
2013-09-02 00:02:25 +0800

    Improved execution efficiency

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ import os, sys, subprocess, pickle
 from MacDisk import *
 
 def main():
-    DMGList = []
-    RAIDSetList = []
-    MountedDMGList = []
+    DMGFileList = []
 
-#    IgnoreList = [] # Debug use
-    IgnoreList = ['disk0'] # Internal disk
+    IgnoreList = []
 
     # Part 0
     DiskUtil = MacDiskutil()
@@ -17,21 +14,18 @@ def main():
     # Part 1
     print 'Current working directory: ', os.getcwd()
     for fileName in os.listdir(os.getcwd()):
-        if '.dmg' in fileName[-4:].lower(): DMGList.append(fileName)
-    print 'Found DMGs: ', DMGList
+        if '.dmg' in fileName[-4:].lower(): DMGFileList.append(fileName)
+    print 'Found DMGs: ', DMGFileList
     
     # Part 2
     DiskList = DiskUtil.getWholeDisks()
 #    print 'All disks: ', DiskList # Debug use
-    RAIDDiskList = DiskUtil.getAppleRAIDDisks()
-    RAIDSetList = DiskUtil.getAppleRAIDSets()
-    MountedDMGList = DiskUtil.getMountedImages()
-    CSDiskList = DiskUtil.getCoreStorageAllDisks()
+    IgnoreList.extend(DiskUtil.getInternalDisks())
+    IgnoreList.extend(DiskUtil.getAppleRAIDAll())
+    # There are other options for RAID, see MacDisk module for details
+    IgnoreList.extend(DiskUtil.getMountedImages())
+    IgnoreList.extend(DiskUtil.getCoreStorageAllDisks())
     
-    for disk in RAIDDiskList: IgnoreList.append(disk)
-    for disk in RAIDSetList: IgnoreList.append(disk)
-    for disk in MountedDMGList: IgnoreList.append(disk)
-    for disk in CSDiskList: IgnoreList.append(disk)
     for disk in set(IgnoreList): DiskList.remove(disk)
     
     print 'Valid disks:'
