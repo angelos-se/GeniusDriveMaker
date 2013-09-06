1be8a8e40bcad319e1bcb5c3725148588434e209
angelos-se <Angelos@Kana.local>
2013-09-07 02:17:36 +0800

    Added encoding and now tries partitioning disks

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
+ # -*- coding: utf-8 -*-
 import os, sys, subprocess, pickle
 from MacDisk import *
 
 def main():
     DMGFileList = []
 
+#    IgnoreList = ['disk4']
     IgnoreList = []
 
     # Part 0
@@ -15,7 +17,9 @@ def main():
     print 'Current working directory: ', os.getcwd()
     for fileName in os.listdir(os.getcwd()):
         if '.dmg' in fileName[-4:].lower(): DMGFileList.append(fileName)
-    print 'Found DMGs: ', DMGFileList
+
+    DMGSizeDict = DiskUtil.getSizeForFiles(DMGFileList)
+    print 'Found DMGs: ', DMGSizeDict
     
     # Part 2
     DiskList = DiskUtil.getWholeDisks()
@@ -31,6 +35,18 @@ def main():
     print 'Valid disks:'
     DiskNameList = DiskUtil.getMediaNameForList(DiskList)
     for key, value in DiskNameList.iteritems():
-        print key, ": ", value 
+        print key, ': ', value 
+
+# Make shift solution
+    for disk in DiskNameList:
+        cmdString = 'diskutil partitionDisk ' + disk + ' GPTFormat '
+        for dmg in DMGFileList:
+            cmdString += 'JHFS+ ' + dmg[:-4] + ' ' + str(DMGSizeDict[dmg] *1.5) + 'B '
+        cmdString += 'JHFS+ Free 1B'
+        print cmdString
+    
+    """sudo asr --source /Volumes/My\ Book\ VelociRaptor\ Duo/Users/apple/Desktop/Scripts/ASD/IMAGES/OS155.dmg --target /Volumes/OS155 --erase --noverify --noprompt"""
+    
+    
 
 if __name__ == "__main__": main()
