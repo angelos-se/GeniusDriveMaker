118519e28bb5fb9b627ec1ae097dd0106dbce6a3
angelos-se <Angelos@Kana.local>
2013-09-09 21:53:17 +0800

    Added launcher and renamed some stuff

--- GDMain.py
+++ GDMain.py
@@ -4,11 +4,12 @@ import os, sys, subprocess
 from MacDisk import *
 
 def main():
-    RPartName = 'Reserved'
-    DMGFileList = []
+    RPartName = 'GDMRsvd'
+    # Spare space on disk will be named using string RPartName, this is also used to identify drives formatted using Genius Drive Maker
 
-    IgnoreList = ['disk99']
-    IgnoreMediaList = ['Hitachi HTS543216L9SA02 Media']
+    # The following is just an example, disk0 is usually automatically ignored when booted from internal disk. If you have disk drives that you want to protect, add the name to IgnoreMediaList and the drive will be automatically excluded.
+    IgnoreList = ['disk98', 'disk99']
+    IgnoreMediaList = ['Hitachi HTS543216L9SA02 Media', 'ADATA USB Flash Drive Media']
 
     # Part 0
     DiskUtil = MacDiskutil() # Required for most of diskutil operation
@@ -17,8 +18,10 @@ def main():
     # Part 1
     print 'Current working directory:', os.getcwd()
 
+    DMGFileList = []
     for fileName in os.listdir(os.getcwd()):
         if '.dmg' in fileName[-4:].lower(): DMGFileList.append(fileName)
+    if len(DMGFileList) == 0: sys.exit("No DMG found, running from the wrong directory?")
     DMGSizeDict = DiskUtil.getSizeForFiles(DMGFileList)
     print 'Found DMGs:', DMGSizeDict
     
@@ -30,28 +33,17 @@ def main():
     # There are other options for RAID, see MacDisk module for details
     IgnoreList.extend(DiskUtil.getMountedImages())
     IgnoreList.extend(DiskUtil.getCoreStorageAllDisks())
-    
-    for disk in set(IgnoreList): DiskList.remove(disk)
+
+    for disk in set(IgnoreList):
+        if disk in DiskList: DiskList.remove(disk)
 
     DiskNameDict = DiskUtil.getMediaNameForList(DiskList).iteritems()
 
-# Make shift solution
-    """for disk in DiskNameList:
-        cmdString = 'diskutil partitionDisk ' + disk + ' GPTFormat '
-        for dmg in DMGFileList:
-            cmdString += 'JHFS+ ' + dmg[:-4] + ' ' + str(DMGSizeDict[dmg] *1.5) + 'B '
-        cmdString += 'JHFS+ Free 1B'
-        print cmdString"""
-    
-    """sudo asr --source /Volumes/My\ Book\ VelociRaptor\ Duo/Users/apple/Desktop/Scripts/ASD/IMAGES/OS155.dmg --target /Volumes/OS155 --erase --noverify --noprompt"""
-    
-# Hopefully this is perm
     for disk, MediaName in DiskNameDict:
         
         diskVolumeDict = DiskUtil.getVolumesForDisk(disk)
         print 'On', MediaName, '('+disk+') found volumes:', diskVolumeDict
-        
-#        print MediaName, '('+disk+') will be erased:', not DiskUtil.diskHasVolume(disk, RPartName)
+
         if not DiskUtil.diskHasVolume(disk, RPartName):
             print MediaName, '('+disk+') will be erased.'
             if 'yes' != raw_input('Proceed? (yes/NO) ').lower(): continue # #Prompt
